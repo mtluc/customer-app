51151615db0ab9b4383dd542f07d1450f04a49a4
mtluc <tienluc0811@gmail.com>
2025-03-08 23:30:48 +0700

    tạo box-search

--- src/components/layout/header/header.tsx
+++ src/components/layout/header/header.tsx
@@ -1,12 +1,13 @@
+"use client"
+import { Button } from '@/components/ui/button'
 import Image from '@/components/ui/image'
-import { Input } from '@/components/ui/input'
 import { LucideSearch } from 'lucide-react'
 import Link from 'next/link'
-import { PropsWithChildren } from 'react'
-import { createPortal } from 'react-dom'
+import { PropsWithChildren, useState } from 'react'
 import SearchBar from '../search-bar/search-bar'
 
 export const Header = ({ children }: PropsWithChildren) => {
+  const [openSearch, setOpenSearch] = useState(false);
   return (
     <>
       <header className="sticky top-0 z-10 bg-primary p-1">
@@ -14,18 +15,17 @@ export const Header = ({ children }: PropsWithChildren) => {
           <Link href="/" className="relative h-12 w-16" title="">
             <Image src="/imgs/logo.svg" width={56} height={42} alt="jbb" className="h-full w-full rounded-md object-contain" />
           </Link>
-          <div className="relative m-1 flex-1">
-            <LucideSearch className="absolute bottom-0 left-3 top-0 my-auto h-5 w-5 text-primary" />
-            <Input
-              type="text"
-              placeholder="Nhập tên sản phẩm..."
-              className="rounded-full border-none bg-white pl-10 pr-4 text-gray-800 shadow-[inset_0_0_4px_rgba(0,0,0,0.5)] placeholder:italic placeholder:text-primary focus:outline-none"
-            />
-          </div>
+          <Button
+            className="relative m-1 flex-1 rounded-full border-none bg-white px-4 text-gray-800 shadow-[inset_0_0_4px_rgba(0,0,0,0.5)] focus:outline-none text-primary text-left font-normal hover:bg-white"
+            onClick={() => { setOpenSearch(true) }}
+          >
+            <LucideSearch className="top-0 my-auto size-5" />
+            <span className='flex-1 italic'>Nhập tên sản phẩm...</span>
+          </Button>
         </div>
         {children}
       </header>
-      <SearchBar />
+      <SearchBar open={openSearch} onOpenChanged={(s) => setOpenSearch(s)} />
     </>
   )
 }
--- src/components/layout/search-bar/search-bar.tsx
+++ src/components/layout/search-bar/search-bar.tsx
@@ -1,31 +1,79 @@
+"use client"
+import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle } from "@/components/ui/sheet";
-import { LucideArrowLeft } from "lucide-react";
+import { LucideChevronLeft, LucideSearch, LucideX } from "lucide-react";
+import { ChangeEvent, useEffect, useRef, useState } from "react";
 
-const SearchBar = () => {
+interface SearchBarProps {
+    open: boolean,
+    onOpenChanged: (open: boolean) => void
+}
+
+const SearchBar = ({ open, onOpenChanged }: SearchBarProps) => {
+    const inputRef = useRef<HTMLInputElement>(null);
+    const [showClear, setShowClear] = useState(false);
+    useEffect(() => {
+        if (open) {
+            setTimeout(() => {
+                requestAnimationFrame(() => {
+                    inputRef?.current?.focus?.();
+                })
+
+            }, 100);
+        }
+    }, [inputRef, open])
+
+    const textChanged = (e: ChangeEvent<HTMLInputElement>) => {
+        setShowClear(e.target.value ? true : false);
+    }
+
+    const clear = () => {
+        requestAnimationFrame(() => {
+            inputRef?.current?.focus?.();
+        })
+    }
     return (
-        <Sheet open={true}>
+        <Sheet open={open} onOpenChange={(s) => onOpenChanged(s)}>
             <SheetContent
                 side="top"
                 className="h-screen w-screen p-0 bg-gray-500 shadow-xl !duration-200"
-                showBtnClose={true}
+                showBtnClose={false}
             >
-                <SheetHeader className="space-y-0 bg-primary">
+                <SheetHeader className="space-y-0 bg-primary py-2">
                     <SheetTitle className="hidden" />
                     <SheetDescription className="hidden" />
-                    <div className="m-auto max-w-4xl w-full flex items-center">
-                        <LucideArrowLeft/>
-                        <div className="flex-1">
+                    <div className="m-auto max-w-4xl w-full flex items-center px-2">
+                        <Button variant='ghost' className="p-0 mx-4 hover:bg-transparent" onClick={() => onOpenChanged(false)}>
+                            <LucideChevronLeft className="stroke-1 !size-8 text-primary-foreground" />
+                        </Button>
+                        <form action="/auction" className="flex-1 relative">
+                            <Button variant='ghost' type="submit" className="absolute top-1/2 left-2 -translate-y-1/2 p-0 h-auto hover:bg-transparent">
+                                <LucideSearch className="!size-6 text-primary stroke-1" />
+                            </Button>
                             <Input
+                                ref={inputRef}
                                 type="text"
                                 name="keyword"
-                                className="bg-primary-foreground rounded-full" 
-                                placeholder="Nhập tên sản phẩm cần tìm..."/>
-                        </div>
+                                className={"bg-primary-foreground rounded-full pl-10 stroke-1 bg-white placeholder:italic " +
+                                    (showClear ? ' pr-10' : '')
+                                }
+                                placeholder="Nhập tên sản phẩm cần tìm..."
+                                onChange={(e) => textChanged(e)}
+                                autoFocus
+                            />
+                            {
+                                showClear &&
+                                <Button variant='ghost' type="reset" className="absolute top-1/2 right-2 -translate-y-1/2 p-0 h-auto hover:bg-transparent"
+                                    onClick={() => clear()}>
+                                    <LucideX className="!size-6 text-red-500 stroke-1" />
+                                </Button>
+                            }
+                        </form>
                     </div>
                 </SheetHeader>
 
-                <div className="m-auto max-w-4xl w-full">
+                <div className="m-auto max-w-4xl w-full h-full bg-white px-2">
 
 
                     <h2 className="text-xl font-bold">Sheet Toàn Màn Hình</h2>
