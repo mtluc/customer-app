a180e57bdbcef2284fe7b341137b07966e947330
mtluc <tienluc0811@gmail.com>
2025-03-09 10:24:55 +0700

    thêm ảnh cho top keysearch

--- src/components/home/top-search/top-search.tsx
+++ src/components/home/top-search/top-search.tsx
@@ -7,7 +7,7 @@ import { useEffect } from "react"
 
 const TopSearch = () => {
   injectReducer(topSearchSlice)
-  const keyWords = useSelectSlice(topSearchSlice, (s) => s.data)
+  const keywords = useSelectSlice(topSearchSlice, (s) => s.data)
   useEffect(() => {
     injectReducer(topSearchSlice)
     return () => {
@@ -22,14 +22,14 @@ const TopSearch = () => {
           Xem những gì mọi người đang tìm kiếm
         </div>
         <div className="-mx-1 flex flex-wrap py-2">
-          {keyWords.map((keyword, i) => {
+          {keywords.ids.map((id) => {
             return (
               <Button
                 asChild
-                key={i}
+                key={id}
                 variant={'ghost'}
                 className="m-1 h-auto rounded-full px-2.5 py-1 bg-gray-100 shadow-sm hover:text-primary-foreground hover:bg-primary transition-all duration-300">
-                <Link href={`/auction?key=${encodeURIComponent(keyword)}`}> {keyword}</Link>
+                <Link href={`/auction?key=${encodeURIComponent(id)}`}> {keywords.data[id].keyword}</Link>
               </Button>
             )
           })}
