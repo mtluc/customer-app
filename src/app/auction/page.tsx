c55f28e01edb016a22816838614abba5b427f795
mtluc <tienluc0811@gmail.com>
2025-03-13 00:29:36 +0700

    sửa lỗi build

--- src/app/auction/page.tsx
+++ src/app/auction/page.tsx
@@ -1,13 +1,10 @@
 import { Metadata } from 'next'
+import { PageProps } from '../../../.next/types/app/page'
 
 export const metadata: Metadata = {
   title: 'Tìm kiếm sản phẩm đấu giá - JBB',
   description: 'Mô tả trang web của bạn'
 }
-export default async function AuctionSearchPage({
-  searchParams
-}: {
-  searchParams: URLSearchParams
-}) {
+export default async function AuctionSearchPage({ searchParams }: PageProps) {
   return <>{JSON.stringify(await searchParams)}</>
 }
