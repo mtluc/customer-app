a4e03ba3584c6e611db7c8ed056022dc9bff6643
mtluc <tienluc0811@gmail.com>
2025-03-20 23:35:30 +0700

    check code trang chi tiết sản phẩm đấu giá

--- src/components/auction/auction-detail/auction-detail.tsx
+++ src/components/auction/auction-detail/auction-detail.tsx
@@ -1,10 +1,50 @@
+import { Button } from '@/components/ui/button'
 import { IAuctionDetail } from '@/store/slices/auction/auction-detail.Slice'
+import { LucideHeart, LucideTruck } from 'lucide-react'
 import AuctionImageBox from './image-box'
 
 const AuctionDetail = ({ item }: { item: IAuctionDetail }) => {
   return (
     <>
       <AuctionImageBox images={item.images} />
+      <div className="flex bg-primary p-2 text-primary-foreground">
+        <div>
+          <div>Giá hiện tại</div>
+          <div className="text-2xl font-semibold">¥ 681,000</div>
+        </div>
+        <div className="flex-1"></div>
+        <div className="text-right">
+          <div>Kết thúc trong</div>
+          <div>01 ngày 22:16:40</div>
+          <div>314 Lượt đấu</div>
+        </div>
+      </div>
+      <div className="bg-background p-2">
+        <div className="flex items-start">
+          <h1 className="flex-1 font-semibold">{item.name}</h1>
+          <Button className="h-auto p-0" variant="ghost">
+            <LucideHeart className="!size-8" />
+          </Button>
+        </div>
+        <div className="mt-4 flex items-center">
+          <div className="flex-1">
+            <div>Giá mua ngay:</div>
+            <div className="text-2xl font-semibold">¥ 1,681,000</div>
+          </div>
+          <div>
+            <Button variant="outline" className="border-primary text-primary">
+              MUA NGAY
+            </Button>
+          </div>
+        </div>
+        <div className="mt-4 flex rounded-md bg-orange-100 p-2">
+          <LucideTruck />
+          <div>
+            <div>Phí vận chuyển nội địa</div>
+            <div>Không</div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
--- src/components/auction/auction-detail/image-box.tsx
+++ src/components/auction/auction-detail/image-box.tsx
@@ -1,7 +1,9 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
+//* eslint-disable @typescript-eslint/no-explicit-any */
 'use client'
+import { Button } from '@/components/ui/button'
 import {
   Carousel,
+  CarouselApi,
   CarouselContent,
   CarouselItem,
   CarouselNext,
@@ -14,13 +16,16 @@ import {
   DialogTitle
 } from '@/components/ui/dialog'
 import Image from '@/components/ui/image'
-import { useState } from 'react'
+import { LucideX } from 'lucide-react'
+import { useEffect, useState } from 'react'
+import { createPortal } from 'react-dom'
 
 const AuctionImageBox = ({ images }: { images: string[] }) => {
-  const [index, setIndex] = useState(0)
   const [isFullScreen, setIsFullScreen] = useState(false)
   const [isSwiping, setIsSwiping] = useState(false)
   const [touchStart, setTouchStart] = useState(0)
+  const [api, setApi] = useState<CarouselApi>()
+  const [current, setCurrent] = useState(0)
 
   const handleTouchStart = (e: React.TouchEvent) => {
     setTouchStart(e.touches[0].clientX)
@@ -33,69 +38,112 @@ const AuctionImageBox = ({ images }: { images: string[] }) => {
     }
   }
 
-  const handleTouchEnd = (e: React.TouchEvent, i: number) => {
+  const handleTouchEnd = () => {
     if (!isSwiping) {
-      setIndex(i)
       setIsFullScreen(true)
     }
   }
 
+  useEffect(() => {
+    if (!api) {
+      return
+    }
+
+    setCurrent(api.selectedScrollSnap() + 1)
+
+    api.on('select', () => {
+      setCurrent(api.selectedScrollSnap() + 1)
+    })
+  }, [api])
+
   return (
-    <div className="flex w-full max-w-lg flex-col items-center gap-4">
-      <Carousel opts={{ loop: true }} className="w-full">
+    <div>
+      <Carousel opts={{ loop: true }} className="w-full" setApi={setApi}>
         <CarouselContent>
           {images.map((img, i) => (
             <CarouselItem
               key={i}
-              className="relative h-96 w-full"
+              className="relative w-full"
               onTouchStart={handleTouchStart}
               onTouchMove={handleTouchMove}
-              onTouchEnd={(e) => handleTouchEnd(e, i)}
+              onTouchEnd={handleTouchEnd}
               onClick={() => setIsFullScreen(true)}
             >
               <Image
                 src={img}
                 alt="Product Image"
-                fill
-                className="cursor-pointer rounded-xl"
+                width={400}
+                height={400}
+                className="aspect-square w-full object-cover"
               />
             </CarouselItem>
           ))}
         </CarouselContent>
-        <CarouselPrevious />
-        <CarouselNext />
+        <div className="absolute bottom-2 left-2 rounded bg-gray-800 bg-opacity-50 p-2 py-1 text-primary-foreground">
+          {current}/{images.length}
+        </div>
       </Carousel>
 
+      {isFullScreen &&
+        createPortal(
+          <div className="fixed bottom-0 left-0 right-0 top-0 z-50 flex items-center bg-black bg-opacity-80 align-middle">
+            <Button
+              className="absolute right-4 top-4 p-0 text-primary-foreground"
+              variant={'ghost'}
+              onClick={() => setIsFullScreen(false)}
+            >
+              <LucideX className="!size-8" />
+            </Button>
+            <div className="m-auto max-h-full max-w-4xl">
+              <Carousel
+                opts={{ loop: true, startIndex: current }}
+                className="w-full"
+              >
+                <CarouselContent>
+                  {images.map((img, i) => (
+                    <CarouselItem key={i} className="relativew-full">
+                      <Image
+                        src={img}
+                        alt="Product Image Fullscreen"
+                        width={600}
+                        height={600}
+                        className="aspect-square w-full object-cover"
+                      />
+                    </CarouselItem>
+                  ))}
+                </CarouselContent>
+                <CarouselPrevious className="fixed left-0" />
+                <CarouselNext className="fixed right-0" />
+              </Carousel>
+            </div>
+          </div>,
+          document.body
+        )}
       {/* Dialog để hiển thị ảnh full màn hình với Carousel */}
-      <Dialog open={isFullScreen} onOpenChange={setIsFullScreen}>
-        <DialogContent>
+      <Dialog open={isFullScreen && false} onOpenChange={setIsFullScreen}>
+        <DialogContent className="w-full max-w-4xl border-none p-0">
           <DialogTitle />
           <DialogDescription />
-          <div className="fixed inset-0 flex w-full items-center justify-center bg-black bg-opacity-90">
+          <div className="inset-0 flex w-full items-center justify-center bg-black bg-opacity-90">
             <Carousel
-              opts={{ loop: true, startIndex: index }}
-              className="w-full max-w-4xl"
+              opts={{ loop: true, startIndex: current }}
+              className="w-full"
             >
               <CarouselContent>
                 {images.map((img, i) => (
-                  <CarouselItem key={i} className="relative h-[80vh] w-full">
+                  <CarouselItem key={i} className="relativew-full">
                     <Image
                       src={img}
                       alt="Product Image Fullscreen"
-                      fill
-                      className="rounded-xl"
+                      width={600}
+                      height={600}
+                      className="aspect-square w-full object-cover"
                     />
                   </CarouselItem>
                 ))}
               </CarouselContent>
-              <CarouselPrevious
-                onClick={() =>
-                  setIndex((prev) => (prev - 1 + images.length) % images.length)
-                }
-              />
-              <CarouselNext
-                onClick={() => setIndex((prev) => (prev + 1) % images.length)}
-              />
+              <CarouselPrevious className="fixed left-0" />
+              <CarouselNext className="fixed right-0" />
             </Carousel>
           </div>
         </DialogContent>
